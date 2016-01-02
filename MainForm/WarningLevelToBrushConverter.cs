6ea4daf6a63e8ade1aa9c3574e89614072ae1420
CuteITGuy <grisetroseciel@gmail.com>
2016-01-02 16:10:27 +0700

    First commit

---
+++ MainForm/WarningLevelToBrushConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+using Todos;
+
+
+namespace MainForm
+{
+    public class WarningLevelToBrushConverter: IValueConverter
+    {
+        private readonly Color _startColor = Colors.White;
+        private const double ANGLE = 90.002;
+
+
+        #region Methods
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is WarningLevel)) return DependencyProperty.UnsetValue;
+            var warningLevel = (WarningLevel)value;
+            switch (warningLevel)
+            {
+                case WarningLevel.Overdue:
+                    return new LinearGradientBrush(_startColor, Color.FromRgb(255, 147, 147), ANGLE);
+                case WarningLevel.InProgress:
+                    return new LinearGradientBrush(_startColor, Color.FromRgb(204, 148, 255), ANGLE);
+                case WarningLevel.Approaching:
+                    return new LinearGradientBrush(_startColor, Color.FromRgb(255, 242, 148), ANGLE);
+                case WarningLevel.Normal:
+                    return new LinearGradientBrush(_startColor, Color.FromRgb(149, 255, 160), ANGLE);
+                case WarningLevel.None:
+                    return new LinearGradientBrush(_startColor, Color.FromRgb(184, 184, 184), ANGLE);
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+        #endregion
+    }
+}
---
+++ Todos/Todo.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using CB.Data;
+
+
+namespace Todos
+{
+    public class Todo: ObservableObject
+    {
+        #region Fields
+        private const int IN_PROGRESS_HOURS = 2,
+                          APPROACHING_HOURS = 24;
+
+        private string _content = "Task 1";
+        private DateTime _deadline = DateTime.Now;
+        private bool _isDone;
+
+        [NonSerialized]
+        private WarningLevel _warningLevel;
+        #endregion
+
+
+        #region  Properties & Indexers
+        public string Content
+        {
+            get { return _content; }
+            set { SetProperty(ref _content, value); }
+        }
+
+        public DateTime Deadline
+        {
+            get { return _deadline; }
+            set
+            {
+                SetProperty(ref _deadline, value);
+                SetWarningLevel();
+            }
+        }
+
+        public bool IsDone
+        {
+            get { return _isDone; }
+            set
+            {
+                SetProperty(ref _isDone, value);
+                SetWarningLevel();
+            }
+        }
+
+        public int Priority { get; set; } = 1;
+
+        public WarningLevel WarningLevel => _warningLevel;
+        #endregion
+
+
+        #region Implementation
+        private void SetWarningLevel()
+        {
+            var hoursToDeadline = (Deadline - DateTime.Now).TotalHours;
+            var warningLevel = IsDone
+                                   ? WarningLevel.None
+                                   : hoursToDeadline < 0
+                                         ? WarningLevel.Overdue
+                                         : hoursToDeadline < IN_PROGRESS_HOURS
+                                               ? WarningLevel.InProgress
+                                               : hoursToDeadline < APPROACHING_HOURS
+                                                     ? WarningLevel.Approaching : WarningLevel.Normal;
+
+            // ReSharper disable once ExplicitCallerInfoArgument
+            SetProperty(ref _warningLevel, warningLevel, nameof(WarningLevel));
+        }
+        #endregion
+    }
+
+    public enum WarningLevel
+    {
+        Overdue,
+        InProgress,
+        Approaching,
+        Normal,
+        None,
+    }
+}
